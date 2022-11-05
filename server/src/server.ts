4dc7688d4c96cf46261f3b3374b6a63b6b43247f
Aluizio N <aluizio.sln@gmail.com>
2022-11-05 08:21:44 -0400

    mobile setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import Fastify from "fastify";
 import {PrismaClient} from "@prisma/client"
-
+import cors from "@fastify/cors"
 
 const prisma = new PrismaClient({
     log: ["query"],
@@ -10,6 +10,10 @@ async function bootstrap() {
     const fastify = Fastify ({
         logger: true,
     })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
     
     fastify.get('/pools/count', async () => {
         const count = await prisma.pool.count()
@@ -18,7 +22,7 @@ async function bootstrap() {
     })
     
     
-    await fastify.listen({port: 3333})
+    await fastify.listen({port: 3333, /*host: "0.0.0.0"*/})
 }
 
 
